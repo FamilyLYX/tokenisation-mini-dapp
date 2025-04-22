64bbd676fbcf0b095ed6f95740cdfce13005bb92
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-04-22 19:53:45 +0530

    fix the carousel issue

--- packages/nextjs/src/app/product-preview/page.tsx
+++ packages/nextjs/src/app/product-preview/page.tsx
@@ -1,20 +1,28 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { ArrowLeft, ArrowRight } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import { Product } from "@/types";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function ProductPreview() {
   const { push } = useRouter();
   const [product, setProduct] = useState<Product | null>(null);
-  const [index, setIndex] = useState(0);
+  const prevRef = useRef<HTMLButtonElement>(null);
+  const nextRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const storedProduct = localStorage.getItem("product");
     if (storedProduct) {
+      console.log("Stored product:", JSON.parse(storedProduct));
       setProduct(JSON.parse(storedProduct));
     } else {
       toast.error("Please fill the form first, redirecting to form...");
@@ -22,39 +30,34 @@ export default function ProductPreview() {
     }
   }, []);
 
-  const handleNext = () => {
-    if (product) {
-      setIndex((index + 1) % product.images.length);
-    }
-  };
-
-  const handlePrev = () => {
-    if (product) {
-      setIndex((index - 1 + product.images.length) % product.images.length);
-    }
-  };
-
   if (!product) return <div className="text-center p-10">Loading...</div>;
 
   return (
     <div className="min-h-screen flex flex-col justify-between px-6 py-6">
       <h2 className="text-xl font-[cursive] italic text-center mb-4">family</h2>
 
-      {/* Image Carousel */}
-      <div className="relative rounded-2xl overflow-hidden">
-        <button
-          onClick={handlePrev}
-          className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-white/80 p-1 rounded-full"
-        >
-          <ArrowLeft size={16} />
-        </button>
-        <button
-          onClick={handleNext}
-          className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-white/80 p-1 rounded-full"
-        >
-          <ArrowRight size={16} />
-        </button>
-      </div>
+      <Carousel
+        className="mx-auto mb-16 w-4/5"
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {product.images.map((product, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/1">
+              {/* eslint-disable @next/next/no-img-element */}
+              <img
+                src={product}
+                alt={`Product image ${index + 1}`}
+                className="w-full object-fit max-h-[400px] rounded-lg shadow-lg"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious ref={prevRef} />
+        <CarouselNext ref={nextRef} />
+      </Carousel>
 
       <div className="mt-6 text-center">
         <p className="text-sm mt-2 px-2 text-gray-500 font-mono leading-relaxed">
