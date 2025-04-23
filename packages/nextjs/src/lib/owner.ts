5282b222f4e0bb93686e4bf0364560ec47268f43
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-04-23 23:53:10 +0530

    fix vercel deployment + all working

--- packages/nextjs/src/lib/owner.ts
+++ packages/nextjs/src/lib/owner.ts
@@ -3,7 +3,8 @@ import { privateKeyToAccount } from "viem/accounts";
 import { FACTORY_ABI, FACTORY_ADDRESS } from "@/constants/factory";
 import { luksoTestnet } from "viem/chains";
 import { Product } from "@/types";
-
+import { NFT_ABI } from "@/constants/dpp";
+import { fromHex } from "viem/utils";
 type createNFTResponse = { hash: string };
 
 if (!process.env.NEXT_PUBLIC_PRIVATE_KEY) {
@@ -61,13 +62,31 @@ export async function testCreateNFT(
 export async function getAllNFTMetadata(): Promise<number> {
   try {
     // 1. Fetch deployed NFTs from the factory contract
-    const deployedNFTs = await readClient.readContract({
+    const deployedNFTs = (await readClient.readContract({
       abi: FACTORY_ABI,
       address: FACTORY_ADDRESS,
       functionName: "getDeployedNFTs",
-    });
-
-    return deployedNFTs ? deployedNFTs?.length : 0;
+    })) as string[];
+    console.log("Deployed NFTs:", deployedNFTs);
+    for (const nftAddress of deployedNFTs) {
+      console.log("NFT Address:", nftAddress);
+      // 2. Fetch metadata for each NFT
+      const metadata = await readClient.readContract({
+        abi: NFT_ABI,
+        address: nftAddress as `0x${string}`,
+        functionName: "getPublicMetadata",
+      });
+      const decodedMetadata = JSON.parse(
+        fromHex(metadata as `0x${string}`, "string"),
+      );
+      const owner = await readClient.readContract({
+        abi: NFT_ABI,
+        address: nftAddress as `0x${string}`,
+        functionName: "owner",
+      });
+      console.log("👤 Owner:", owner, " of metadata: ", decodedMetadata);
+    }
+    return deployedNFTs.length;
   } catch (error) {
     console.error("Error fetching NFT metadata:", error);
     throw error;
