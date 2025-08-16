ced296989021f4538e30d01702b36487bc02dd2a
Azeez Mumeen <azeezmumeen6@gmail.com>
2025-08-16 10:35:06 +0100

    prepre for vercel deployment

--- packages/nextjs/src/app/api/upload/route.ts
+++ packages/nextjs/src/app/api/upload/route.ts
@@ -1,4 +1,3 @@
-"use server";
 import { NextRequest, NextResponse } from "next/server";
 import { uploadImageToS3 } from "@/lib/s3";
 
@@ -12,19 +11,13 @@ export async function POST(req: NextRequest) {
 
   const arrayBuffer = await file.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
-  const credentialsTest = {
-    accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY_ID ? "present" : "missing",
-    secretAccessKey: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY
-      ? "present"
-      : "missing",
-  };
 
   try {
     const url = await uploadImageToS3(buffer, file.name, file.type);
     return NextResponse.json({ url });
   } catch (err) {
     return NextResponse.json(
-      { error: "Upload failed", details: err, credentialsTest },
+      { error: "Upload failed", details: err },
       { status: 500 }
     );
   }
